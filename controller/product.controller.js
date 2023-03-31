111d6f8de4efd89e3cd62b9c7f5d1da73d9162a2
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-03-31 15:18:44 +0530

    sort filter 1.0.0

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,10 +1,18 @@
 const { productModel } = require("../model/product.model");
 
 const getAllProcucts = async (req, res) => {
-  const { page } = req.query;
-  const pageNo = (page-1) * 18
+  const { page, sort } = req.query;
+  const pageNo = (page - 1) * 18;
+  let sortFilter = {};
+  if (sort) {
+    sortFilter.newprice = sort;
+  }
   try {
-    const productsData = await productModel.find().skip(pageNo).limit(18);
+    const productsData = await productModel
+      .find()
+      .sort(sortFilter)
+      .skip(pageNo)
+      .limit(18);
     if (productsData.length == 0) {
       return res.status(404).json({ msg: "products not found" });
     } else {
