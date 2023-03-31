5348ab981f78b08005a511d5e896b37e61b4ff4c
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-03-31 13:28:08 +0530

    pagingation 1.1.1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
-app.use(authCheck);
+// app.use(authCheck);
 app.use("/products", productsRouter);
 
 app.listen(process.env.port, () => {
