def95715faae0b9507ab3b88e85c81094ff5f112
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-04-02 20:02:27 +0530

    add to bag 1.0.1

--- controller/bag.controller.js
+++ controller/bag.controller.js
@@ -4,11 +4,11 @@ const jwt = require("jsonwebtoken");
 const add_to_bag = async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = jwt.verify(token, "fw24_605");
-  req.body.userId = decoded.userId
+  req.body.userId = decoded.userId;
   const bag_data = new bagModel(req.body);
   try {
     await bag_data.save();
-    return res.status(201).json({ msg: "added to cart successfully" });
+    return res.status(201).json({ success: "added to cart successfully" });
   } catch (error) {
     return res.status(400).json({ err: error });
   }
