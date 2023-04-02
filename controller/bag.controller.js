792a63a2605066f92a22d036cbd7b24fb60513fe
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-04-02 23:02:16 +0530

    remove form bag

--- controller/bag.controller.js
+++ controller/bag.controller.js
@@ -28,6 +28,13 @@ const get_bags = async (req, res) => {
     return res.status(400).json({ err: error });
   }
 };
-const delete_bag = async (req, res) => {};
+const delete_bag = async (req, res) => {
+  try {
+    bagModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ msg: "removed form bag" });
+  } catch (error) {
+    return res.status(400).json({ err: error });
+  }
+};
 
 module.exports = { add_to_bag, get_bags, delete_bag };
